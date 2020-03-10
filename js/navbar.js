89e9d8f525494538192b195c2e208f58074ee8d5
Daniel Olaya <d_olaya@hotmail.com>
2020-03-10 15:13:41 -0700

    WIP: Navbar transition, fixed colors

--- js/navbar.js
+++ js/navbar.js
@@ -6,14 +6,13 @@ $(document).ready(function(){
         $(document).scroll(function(){
             scroll_start = $(this).scrollTop();
             if(scroll_start > offset.top) {
-                console.log('I am in if');
-                $(".navbar").css('background', 'purple');
 
-                // $(".navbar-default").css('background', 'rgba(255,255,255,0.8)');
-                $(".navbar").css('color', 'purple');
+                $(".navbar-default").css('background', 'rgba(255,255,255,0.8)');
+                $(".navbar-brand").css('color', '#111111');
+
+                // $(".navbar").css('color', 'purple');
 
             } else {
-                console.log('else....');
                 $(".navbar-default").css('background', 'transparent');
                 $(".navbar-brand").css('color', '#4a4a4a');
             }
