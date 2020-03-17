7f73c654e597a1d3b0807b4884d194620bc64f77
Daniel Olaya <d_olaya@hotmail.com>
2020-03-16 19:41:24 -0700

    Brick texture for succulent

--- js/offset.js
+++ js/offset.js
@@ -1,39 +1,17 @@
-
-
-
-  var divId;
-
-  $('.nav-link').click(function(){
+// Accomodates for fixed navbar height when making href jumps
+var divId;
+$('.nav-link').click(function(){
     divId = $(this).attr('href');
-     $('html, body').animate({
-      scrollTop: $(divId).offset().top - 75
+    $('html, body').animate({
+        scrollTop: $(divId).offset().top - 75
     }, 100);
-  });
-
-
-$(document).ready(function() {
-    $('#leer').click(function () {
-        $('#conoce-mas').toggleClass("active");
-        $('#conoce-menos').toggleClass("active");
-        console.log("just toggled classes!")
-        //
-        // if ($(this).hasClass("active")) {
-        //     $(this).text("Leer más");
-        // } else {
-        //     $(this).text("Leer menos");
-        // }
-
-    });
 });
 
-// $(document).ready(function() {
-//
-//
-// $('[data-toggle="collapse"]').click(function() {
-//   $(this).toggleClass( "active" );
-//   if ($(this).hasClass("active")) {
-//     $(this).text("Hide");
-//   } else {
-//     $(this).text("Show");
-//   }
-// });
+
+// Expands about me section
+$(document).ready(function() {
+    $('#conoce').click(function () {
+        $('#conoce-mas').toggleClass("active");
+        $('#conoce-menos').toggleClass("active");
+    });
+});
