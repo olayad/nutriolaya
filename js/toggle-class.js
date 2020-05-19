c76a99d27daec21047da0f780b8f8ca91d5f4c42
Daniel Olaya <daniel.olayam@gmail.com>
2020-05-18 18:37:24 -0700

    Refactor and clean-up

---
+++ js/toggle-class.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.conoce').click(function () {
+        $('.conoce-mas').toggleClass("active");
+        $('.conoce-menos').toggleClass("active");
+    });
+});
