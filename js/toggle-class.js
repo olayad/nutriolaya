87f8483f29a355ca6708dd36fe2a531c1a0f6f9f
Daniel Olaya <daniel.olayam@gmail.com>
2020-05-26 15:46:57 -0700

    IOS devices fix

--- js/toggle-class.js
+++ js/toggle-class.js
@@ -8,3 +8,17 @@ function toggleLeer(event){
         $(spans[i]).toggleClass('active');
     }
 }
+
+// Fixed background does not work in IOS devices
+let isIOS = (/iPad|iPhone|iPod/.test(navigator.platform) ||
+(navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) &&
+!window.MSStream
+
+console.log("isIOS:", typeof isIOS);
+if (isIOS === true){
+   console.log('IOS device, switching to background scroll.');
+   // $(".paral-vine").toggleClass("test");
+   $(".paral-vine").toggleClass("paral-fixed");
+   $(".paral-vine").toggleClass("paral-scroll");
+   console.log($(".paral-vine"));
+}
